d2bdd78f2d8edb85cdadc1b41d0266fc7ee4c5b4
Tina Bruce <tina@fishface.se>
2024-04-08 23:03:24 +0200

    Search working

--- script.js
+++ script.js
@@ -3,31 +3,41 @@ const output = document.querySelector('#output');
 
 
 function fetchCharacter(name) {
+    output.innerHTML = `<textarea cols="40" rows="10" readonly></textarea>`;
+
     let response = {};
 
     fetch(`https://www.swapi.tech/api/people/?name=${name}`)
-    .then(res => res.json())
-    .then(data => {
-        //console.log(data);
-        response = data.result[0].properties;
-
-        output.innerHTML = `
-        <h3>Here are the results for ${name}</h3>
-        <textarea
-        cols="40"
-        rows="10"
-        readonly>
-            Height: ${response.height}\n 
-            Mass: ${response.mass}\n 
-            Gender: ${response.gender}\n 
-            Hair color: ${response.hair_color}\n 
-        </textarea>
-        `
+    .then(res => {
+            if(res.ok)
+                return res.json();
+            throw new Error('Error when fetching') 
         })
+    .then(data => {
+        if(data.result.length == 0) {
+            output.innerHTML = `
+            <textarea cols="40" rows="10" readonly>
+            No such character in the universe!
+            </textarea>
+            `
+        }
+        else {
+            response = data.result[0].properties;
+            output.innerHTML = `
+                <h3>Here are the results for ${response.name}</h3>
+                <textarea cols="40" rows="10" readonly>
+                Height: ${response.height}\n 
+                Mass: ${response.mass}\n 
+                Gender: ${response.gender}\n 
+                Hair color: ${response.hair_color}\n 
+                </textarea>
+            `
+        }
+    })
     .catch(err => console.log(err))
 }
 
-form.addEventListener('submit', async (event) => {
+form.addEventListener('submit', (event) => {
     event.preventDefault();
     const characterName = form['character'].value;
 
